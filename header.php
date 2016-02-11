fe095e376a99af561745ceb908145bc19c4cdc64
Matthew Sedlar <sedlarm@gmail.com>
2016-02-11 13:44:15 -0500

    Raleway bold support

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 <![endif]-->
 
 <?php wp_head(); ?>
-<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Raleway:400,500,600,700,800,900' rel='stylesheet' type='text/css'>
 </head>
 
 <body <?php body_class(); ?>>
